e2ca6e3bc52def09206403a1dbb1e7d16575fdbc
Derek Adlai Trujillo <91101091+datrujil@users.noreply.github.com>
2025-01-12 23:48:21 -0800

    Implemented initiation of values, movement, and actions

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,16 +13,56 @@ public class Player : MonoBehaviour
 
     private void Start ()
     {
-        
+        // Initiate starting UI values
+        _numSeedsLeft = _numSeeds;
+        _numSeedsPlanted = 0;
+        _plantCountUI.UpdateSeeds(_numSeedsLeft, _numSeedsPlanted);
     }
 
     private void Update()
     {
-        
+        Vector3 pos = transform.position;
+
+        // Player Movement
+        if (Input.GetKey(KeyCode.W))
+        {
+            pos.y += _speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            pos.y -= _speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            pos.x += _speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            pos.x -= _speed * Time.deltaTime;
+        }
+
+        // Player Actions
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Check if there are available seeds to plant
+            if (_numSeedsLeft > 0)
+            {
+                // Instantiate Plant Prefab
+                Instantiate(_plantPrefab, new Vector3(pos.x, pos.y, 2), Quaternion.identity);
+
+                PlantSeed();
+            }
+            
+        }
+
+        transform.position = pos;
     }
 
     public void PlantSeed ()
     {
-        
+        // Update UI values
+        _numSeedsLeft--;
+        _numSeedsPlanted++;
+        _plantCountUI.UpdateSeeds(_numSeedsLeft, _numSeedsPlanted);
     }
 }
